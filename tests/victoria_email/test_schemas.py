657a88d60a51f0a36147e7d37208c5d9fe3fcfd6
karym <karymaidary@gmail.com>
2020-10-21 00:03:39 +0300

    unit tests for generate random data

--- tests/victoria_email/test_schemas.py
+++ tests/victoria_email/test_schemas.py
@@ -3,6 +3,7 @@ import victoria_email.schemas as schemas
 import yaml
 import marshmallow
 
+
 def test_make_config_single_tenant():
     # Arrange
     with open("./tests/victoria_email/test_single_tenant.yaml", "r") as f:
@@ -28,17 +29,6 @@ def test_make_config_multiple_tenants():
         assert len(ret.load_test.tenant_ids) == 2
 
 
-def test_make_config_no_tenants():
-    # Arrange
-    with open("./tests/victoria_email/test_no_tenant.yaml", "r") as f:
-        file_content = f.read()
-        yaml_obj = yaml.load(file_content)
-        # Act
-        # Assert
-        with pytest.raises(marshmallow.exceptions.ValidationError):    
-            email_schema = schemas.EmailConfigSchema()
-            ret = email_schema.load(yaml_obj)
-
 def test_make_config_with_load_config():
     # Arrange
     with open("./tests/victoria_email/test_with_mutiple_filetypes.yaml", "r") as f:
@@ -49,4 +39,33 @@ def test_make_config_with_load_config():
         ret = email_schema.load(yaml_obj)
         # Assert
         assert len(ret.load_test.load.attachment_count) == 3
-        
+
+
+def test_make_with_generated_random_attachments():
+    with open("./tests/victoria_email/test_with_generated_random_data.yaml", "r") as f:
+        file_content = f.read()
+        yaml_obj = yaml.load(file_content)
+        # Act
+        email_schema = schemas.EmailConfigSchema()
+        ret = email_schema.load(yaml_obj)
+        assert len(ret.load_test.load.attachment_count) == 5
+
+
+def test_make_with_generated_random_tenant_ids():
+    with open("./tests/victoria_email/test_with_generated_random_data.yaml", "r") as f:
+        file_content = f.read()
+        yaml_obj = yaml.load(file_content)
+        # Act
+        email_schema = schemas.EmailConfigSchema()
+        ret = email_schema.load(yaml_obj)
+        assert len(ret.load_test.tenant_ids) == 10
+
+
+def test_make_with_generated_random_attachment_count():
+    with open("./tests/victoria_email/test_with_generated_random_data.yaml", "r") as f:
+        file_content = f.read()
+        yaml_obj = yaml.load(file_content)
+        # Act
+        email_schema = schemas.EmailConfigSchema()
+        ret = email_schema.load(yaml_obj)
+        assert len(ret.load_test.load.attachment_count) == 5
